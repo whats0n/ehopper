3c358444d9a0cf652536c3b0294e1460b54e1333
Kateryna Barynova <kbarinova@getgoing.com>
2017-07-05 15:09:06 +0300

    WSW-511 new customer alternative form

--- src/js/components/_tabs.js
+++ src/js/components/_tabs.js
@@ -8,16 +8,24 @@ export default (function() {
 		if ($this.hasClass(ACTIVE)) return;
 
 		const target = $this.data('tab-open');
+	    const collection = $this.data('tab-collection');
+	    
 		const $parent = $this.closest('[data-tab]');
-		const $buttons = $parent.find('[data-tab-open]');
-		const $sections = $parent.find(`[data-tab-container]`);
-		const $activeSection = $sections.filter(`[data-tab-container="${target}"]`);
+	    let $sections = $parent.find('[data-tab-container]');
+		let $buttons = $parent.find('[data-tab-open]');
+	    
+	    if (collection && collection.length) {
+	    	$sections = $sections.filter(`[data-tab-collection="${collection}"]`);
+	    	$buttons = $buttons.filter(`[data-tab-collection="${collection}"]`);
+	    }
+	    
+			const $activeSection = $sections.filter(`[data-tab-container="${target}"]`);
 
-		$buttons.removeClass(ACTIVE);
-		$this.addClass(ACTIVE);
+			$buttons.removeClass(ACTIVE);
+			$this.addClass(ACTIVE);
 
-		$sections.removeClass(OPEN);
-		$activeSection.addClass(OPEN);
+			$sections.removeClass(OPEN);
+			$activeSection.addClass(OPEN);
 	});
 
 })();
